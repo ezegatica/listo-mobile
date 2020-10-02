02df4d7d519cb66173ed889588b5dd7fc8ad546d
FaustoFang <51834130+FaustoFang@users.noreply.github.com>
2020-10-02 14:02:22 -0300

    Revert "Registrarse semi funca?"
    This reverts commit b5736e0daa96c9797a3fdceb07e35ce1e08e6f0d.

--- routes/searchStack.js
+++ routes/searchStack.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import Signup from '../screens/signup'
+import Test from '../screens/search'
 import { TouchableOpacity, Text, StyleSheet } from 'react-native'
 
 const Stack = createStackNavigator()
@@ -10,7 +10,7 @@ export const SearchStack = ({ navigation }) => {
         <Stack.Navigator  >
             <Stack.Screen name='Inicio' options={{
                 header: () => { null }
-            }} component={Signup} />
+            }} component={Test} />
         </Stack.Navigator>
     )
 }
