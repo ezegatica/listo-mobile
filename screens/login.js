899cb0f2baa998b6d2c7a5280722e544bc03496c
DanShlamo <69364374+DanShlamo@users.noreply.github.com>
2020-10-19 10:58:11 -0300

    sea feliz fausto aqui tiene

--- screens/login.js
+++ screens/login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Button, Text, View, TouchableOpacity, ImageBackground, TextInput, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, Button, Text, View, TouchableOpacity, ImageBackground, TextInput, KeyboardAvoidingView, SafeAreaView } from 'react-native';
 import { globalStyles } from '../styles/global'
 import { auth } from '../api/firebase'
 import { db } from '../api/firebase'
@@ -25,43 +25,66 @@ export function Login({ navigation }) {
   }
 
   return (
-    <View style={globalStyles.container}>
-      <Text>Login</Text>
-      <Text>{errorMessage}</Text>
-      <TextInput
-        style={styles.input}
-        autoCapitalize="none"
-        placeholder="Email"
-        onChangeText={email => setMail(email)}
-      />
-      <TextInput
-        style={styles.input}
-        secureTextEntry
-        autoCapitalize="none"
-        placeholder="Password"
-        onChangeText={password => setPass(password)}
-      />
-      <Button title="Login" onPress={Entrar} />
-      <Button
-        title="Don't have an account? Sign Up"
-        onPress={() => navigation.navigate('Signup')}
-      />
-    </View>
+    <ImageBackground source={BGprueba} style={globalStyles.bg}>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text style={styles.titulo}>Inisiar Sesión</Text>
+          <View style={styles.inputCont}>
+            <Text>{errorMessage}</Text>
+            <TextInput
+              style={styles.input}
+              autoCapitalize="none"
+              placeholder="Email"
+              onChangeText={email => setMail(email)}
+            />
+            <TextInput
+              style={styles.input}
+              secureTextEntry
+              autoCapitalize="none"
+              placeholder="Password"
+              onChangeText={password => setPass(password)}
+            />
+            <Button title="Aceptar" onPress={Entrar} />
+            <Button
+              title="¿Todabía no tenés una cuenta? Registrate"
+              onPress={() => navigation.navigate('Signup')}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  titulo: {
+    marginTop: 50,
+    alignSelf: 'center',
+    fontSize: 30,
+    fontWeight: 'bold'
+
+  },
   input: {
     height: 45,
     backgroundColor: 'rgba(255,255,255,.5)',
     paddingLeft: 10,
-    marginBottom: 25,
-    borderRadius: 5,
+    borderRadius: 20,
     fontSize: 15,
     marginVertical: 20,
+    width: '100%',
+    alignSelf: 'center'
   },
   btns: {
     flex: 2,
     flexDirection: 'row'
   },
+  container: {
+    width: '100%',
+    height: '100%',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+  inputCont: {
+    marginTop: 200
+  }
 })
